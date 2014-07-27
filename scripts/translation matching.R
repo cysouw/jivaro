cc395bc399c69c999204f6b2266811e427076006
Michael Cysouw <cysouw@mac.com>
2014-07-28 01:37:40 +0200

    added more daring clustering of cognates

--- scripts/translation matching.R
+++ scripts/translation matching.R
@@ -3,64 +3,134 @@ library(qlcMatrix)
 library(SnowballC)
 library(igraph)
 
+# Help function for large matrices
+
+doInParts <- function(X, FUN, tol, size) {
+	
+	FUN <- match.fun(FUN)	
+	items <- dim(X)[2]
+	ranges <- seq(0, items, size)
+	if (max(ranges)!=items) { ranges <- c(ranges,items) }
+	chunks <- length(ranges)-1
+	
+	# prepare partition
+	parts <- vector("list",chunks^2)
+	dim(parts) <- c(chunks,chunks)
+	
+	# compute individual parts
+	for (i in 1:chunks) {
+		for (j in 1:i) {
+			tmp <- drop0(
+				FUN(	X[,(ranges[j]+1):ranges[j+1], drop = F],
+						X[,(ranges[i]+1):ranges[i+1], drop = F]
+					), tol = tol)
+			parts[j,i][[1]] <- tmp
+			parts[i,j][[1]] <- t(tmp)
+		}
+	}
+	
+	# bind parts together
+	result <- do.call(cBind,
+			sapply(1:chunks,function(x){do.call(rBind,parts[,x])})
+						)	
+	return(result)	
+}
+
+# == read data ==
+
 data <- read.table("../data.tsv",sep="\t",header=T,quote="",comment.char="")
 
-# ==========
+# == similarity between form of words ==
 
+words <- tokenize(data[,"WORD"],"../orthography profiles/simplified.prf",graphemes="Graphemes",replacements="SCA")
+M <- splitStrings(words, sep = " ")
+BW <- (M$BS*1) %*% (M$SW*1)
+rm(M,words)
+
+system.time(simW <- doInParts(BW, function(X,Y){cosSparse(X,Y,weight=idf)}, tol = 0.3, size = 10e3))
+
+# == similarity between meanings ==
+
+# quick and dirty removal of punctuation
 trans <- stri_extract_words(data[,"TRANSLATION"])
+
+# stemming using SnowballC
 trans <- sapply(trans,function(x){wordStem(x,"spanish")})
-tmp <- ttMatrix(unlist(trans))
-tmp2 <- sparseMatrix(
+
+# type-token matrix of stems
+ST <- ttMatrix(unlist(trans))
+
+# token to translation linkage
+TT <- sparseMatrix(
 			i = rep(1:length(trans),sapply(trans,length)),
 			j = 1:length(unlist(trans))
 		)
-words <- tmp$rownames
-WT <- (tmp$M*1) %*% t(tmp2*1)  
+ST <- (ST$M*1) %*% t(TT*1)  
 
-simT <- drop0(cosSparse(WT,weight="idf"),tol=.3)
-
-# ========
-
-words <- tokenize(data[,"WORD"],"../orthography profiles/simplified.prf",graphemes="Graphemes",replacements="Simplified")
-
-simW11 <- drop0(sim.strings(words[1:15000],sep=" "),tol=.3)
-simW22 <- drop0(sim.strings(words[15001:30000],sep=" "),tol=.3)
-simW33 <- drop0(sim.strings(words[30001:45872],sep=" "),tol=.3)
-
-simW12 <- drop0(sim.strings(words[1:15000],words[15001:30000],sep=" "),tol=.3)
-simW13 <- drop0(sim.strings(words[1:15000],words[30001:45872],sep=" "),tol=.3)
-simW23 <- drop0(sim.strings(words[15001:30000],words[30001:45872],sep=" "),tol=.3)
-
-simW <- rBind(cBind(simW11,simW12,simW13),cBind(t(simW12),simW22,simW23),cBind(t(simW13),t(simW23),simW33))
-
-rownames(simW) <- colnames(simW) <- NULL
-rm(simW11,simW22,simW33,simW12,simW13,simW23)
+system.time(simT <- drop0(cosSparse(ST,weight="idf"),tol=.3))
 
 # ==========
 
 sim <- simW*simT
 
-tmp <- as(drop0(sim,tol=.4),"nMatrix")*1
-g <- graph.adjacency(tmp,mode="undirected",diag=F)
-clus <- clusters(g)
+# communities
+g <- graph.adjacency(sim,mode="undirected",diag=F,weight=T)
+clus <- walktrap.community(g)
+# system.time(fast <- fastgreedy.community(g))
+# system.time(edge <- edge.betweenness.community(g))
 
+tmp <- as(drop0(sim,tol=.4),"nMatrix")*1
+g <- graph.adjacency(tmp,mode="undirected",diag=F,weight=NULL)
+clus <- clusters(g)
+rm(tmp,g)
+
+m <- clus$membership
 clusters <- vector(mode="integer",length=length(words))
-for (i in c(1:clus$no)[clus$csize>1]) {
-	cl <- clus$membership==i
+for (i in c(1:max(m))[table(m)>1]) {
+	cl <- m==i
 	cut <- cutree(hclust(as.dist(-sim[cl,cl]),method="average"),h= -0.05)
 	clusters[cl] <- cut + max(clusters)
 }
 
+# == try combining clusters ==
+
+# similarity between clusters
+
+clus <- clusters
+clus[clus==0] <- c((max(clusters)+1):(max(clusters)+sum(clusters==0)))
+M <- ttMatrix(clus)$M*1
+D <- Diagonal(x = 1/rowSums(M))
+S <- D %*% M %*% sim %*% t(M) %*% D
+
+distr <- as(xtabs(~data[,"SOURCE"]+clus,sparse=T),"nMatrix")
+R <- drop0(1-crossprod(distr))
+
+S <- S*R
+
+#g <- graph.adjacency(S,mode="undirected",diag=F,weight=T)
+#clus2 <- walktrap.community(g)
+#clusters2 <- drop(clus2$membership %*% M)
+
+clus2 <- cutree(hclust(as.dist(-S),method="average"),h= -0.05)
+clusters2 <- drop(clus2 %*% M)
+
+distr2 <- xtabs(~data[,"SOURCE"]+clusters2,sparse=T)
+
+for (i in which(colSums(distr2)==1)) {
+	clusters2[clusters2==i] <- 0
+}
+
 # ===========
 
-data[,"ETYMONID"] <- clusters
-write.table(data,"../data.tsv",sep="\t",row.names=F,quote=F)
+
+data[,"ETYMONID"] <- clusters2
+write.table(data,"../data2.tsv",sep="\t",row.names=F,quote=F)
 
 
 # ===========
-# cl <- clus$membership==1
+# cl <- walk$membership==15
 # tmp <- sim[cl,cl]
 # rownames(tmp) <- data[cl,"WORD"]
-# colnames(tmp) <- data[cl,"TRANSLATION"]
-# plot(hclust(as.dist(-tmp),method="average"),cex=.7)
+# colnames(tmp) <- data[cl,"SOURCE"]
+# plot(hclust(as.dist(-t(tmp)),method="average"),cex=.7)
 # abline(h = -0.05,col="red")
